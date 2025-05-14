4e84b0bc7047b6df2ba4d4e745155cc01fa87e8f
supajbro <supajbro@gmail.com>
2025-05-14 21:46:54 +1000

    Changing colour of h1 via buttons using jQuery

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $("h1").addClass("big-title")
+$("h1").click(function(){
+    $("h1").css("color", "blue");
+})
 
 $(document).ready(function(){
     $('#googleBtn').on('click', function() {
@@ -12,4 +15,8 @@ $(document).ready(function(){
         $('#supajbroBtn').on('click', function() {
         window.open('https://github.com/supajbro?tab=overview&from=2025-05-01&to=2025-05-14', '_blank');
     });
+});
+
+$(".btn").click(function(){
+    $("h1").css("color", "purple");
 });
