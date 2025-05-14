33f0fec61c9ca44d25e8edb58c6f222291f926a6
supajbro <supajbro@gmail.com>
2025-05-14 21:32:27 +1000

    Adding links to buttons via jQuery

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 $("h1").css("color", "red");
+
+$(document).ready(function(){
+    $('#googleBtn').on('click', function() {
+        window.open('https://www.google.com', '_blank');
+    });
+
+    $('#youtubeBtn').on('click', function() {
+    window.open('https://www.youtube.com', '_blank');
+    });
+
+        $('#supajbroBtn').on('click', function() {
+        window.open('https://github.com/supajbro?tab=overview&from=2025-05-01&to=2025-05-14', '_blank');
+    });
+});
